f3efce2d17b1024fb9ca758fd5c22b35aaa02051
unknown <crossbone48@gmail.com>
2023-03-31 13:40:01 +0900

    calurlator

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,64 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState(" ");
+  const [result, setResult] = useState(0);
 
-  const onClickAdd = () => {
-    setCount(count + 1);
-
-    // noUseState = noUseState + 1;
-    // noUseState += 1;
-    // noUseState++;
+  const onChangeNum1 = (e) => {
+    // console.log(e.target.value);
+    setNum1(e.target.value);
   };
-  const onClickSubtract = () => {
-    if (count <= 0) {
-      alert("0보다 못작는다!");
+  const onChangeNum2 = (e) => {
+    setNum2(e.target.value);
+  };
+  const onClickAdd = () => {
+    if (num1 === "" || num2 === "") {
+      alert("값을 입력해주세요");
       return;
     }
-    setCount(count - 1);
+    if (isNaN(num1) || isNaN(num2)) {
+      alert("숫자를 입력해주세요");
+      return;
+    }
+    setResult(parseInt(num1) + parseInt(num2));
   };
-  // setCount((prev)) => {
-  //   return prev -1;
-  // });
 
   return (
-    <div className="bg-red-200 min-h-screen flex flex-col justify-center items-center">
-      <div>{count}</div>
-      <button
-        className="bg-green-300 w-12 h-12 rounded-full"
-        onClick={onClickAdd}
-      >
-        +
-      </button>
-      <button
-        className="bg-blue-300 w-12 h-12 rounded-full mt-3"
-        onClick={onClickSubtract}
-      >
-        -
-      </button>
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-2xl font-bold mb-4">계산기</h1>
+      <div className="text-3xl font-black mb-4">{result}</div>
+      <div className="mb-4">
+        <input
+          className="border-2 focus:outline-none focus:border-purple-400 rounded-lg px-4 py-2 text-xl mr-2"
+          type="text"
+          value={num1}
+          onChange={onChangeNum1}
+        />
+        <input
+          className="border-2 focus:outline-none focus:border-orange-400 rounded-lg px-4 py-2 text-xl ml-2"
+          type="text"
+          value={num2}
+          onChange={onChangeNum2}
+        />
+      </div>
+      <div>
+        <button
+          onClick={onClickAdd}
+          className="border-2 px-2 py-1 rounded-lg border-red-500 text-red-500 ml-4"
+        >
+          Add
+        </button>
+        <button className="border-2 px-2 py-1 rounded-lg border-blue-500 text-blue-500 ml-4">
+          Subtract
+        </button>
+        <button className="border-2 px-2 py-1 rounded-lg border-yellow-500 text-yellow-500 ml-4">
+          Multiply
+        </button>
+        <button className="border-2 px-2 py-1 rounded-lg border-green-500 text-green-500 ml-4">
+          Divide
+        </button>
+      </div>
     </div>
   );
 }
